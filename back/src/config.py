f274ddee6265689738abfa09c6ba6625f25516f0
GDelvoye <gau.delvoye@gmail.com>
2025-02-28 19:49:56 +0100

    tests(back): add test for filtering bulk an ddb creation

--- back/src/config.py
+++ back/src/config.py
@@ -1,1 +1,8 @@
-DATA_PATH = "data"
+from pathlib import Path
+
+
+BACK_DIR = Path(__file__).resolve().parent.parent
+DATA_DIR = BACK_DIR.parent / "data"
+SQL_DIR = BACK_DIR.parent / "sql_database"
+
+SQL_DB_PATH = SQL_DIR / "cards.db"
