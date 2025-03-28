af610bdea66291299f2b26973c12527d5dd1a7fd
G Delvoye <gau.delvoye@gmail.com>
2025-03-28 17:22:22 +0100

    feat(front): loadCubeSummary trigger textAnalyza if user query text before.

--- front/src/app/store/effects/cube-summary.effects.ts
+++ front/src/app/store/effects/cube-summary.effects.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { catchError, map, mergeMap, tap } from 'rxjs/operators';
+import { catchError, map, mergeMap, tap, withLatestFrom } from 'rxjs/operators';
 import { of } from 'rxjs';
 import { VisualizationService } from '../../services/visualization.service';
 import {
@@ -10,6 +10,8 @@ import {
 } from '../actions/cube-summary.actions';
 import { Store } from '@ngrx/store';
 import { setCubeSetSelected } from '../actions/user-input.actions';
+import { selectTextAnalysisQuery } from '../selectors/user-input.selector';
+import { analyzeText } from '../actions/text-analysis.actions';
 
 @Injectable()
 export class CubeSummaryEffects {
@@ -25,9 +27,20 @@ export class CubeSummaryEffects {
           setCubeSetSelected({ setSelected: params.setName })
         );
       }),
-      mergeMap(({ params }) =>
+      withLatestFrom(this.store.select(selectTextAnalysisQuery)),
+      mergeMap(([{ params }, query]) =>
         this.cubeService.getOfficialVisualizationDta(params).pipe(
-          map((data) => loadCubeSummarySuccess({ data })),
+          mergeMap((data) => {
+            const actions: any[] = [loadCubeSummarySuccess({ data })];
+            if (query != '') {
+              actions.push(
+                analyzeText({
+                  params: { text: query, setName: params.setName },
+                })
+              );
+            }
+            return actions;
+          }),
           catchError((error) => of(loadCubeSummaryFailure({ error })))
         )
       )
