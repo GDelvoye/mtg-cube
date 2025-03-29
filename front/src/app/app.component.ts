8fac722a5422f3254fb4e5ef3cadc99861e6bdc9
G Delvoye <gau.delvoye@gmail.com>
2025-03-29 23:23:05 +0100

    refactor(front): useless dependancy.

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { LoadCubeSummaryButtonComponent } from './load-cube-summary-button/load-cube-summary-button.component';
 import { CubeSummaryDisplayComponent } from './cube-summary-display/cube-summary-display.component';
 import { CubeSummarySelectorComponent } from './cube-summary-selector/cube-summary-selector.component';
 import { TextAnalysisInputComponent } from './text-analysis-input/text-analysis-input.component';
@@ -9,8 +7,6 @@ import { TextAnalysisDisplayComponent } from './text-analysis-display/text-analy
 @Component({
   selector: 'app-root',
   imports: [
-    RouterOutlet,
-    LoadCubeSummaryButtonComponent,
     CubeSummaryDisplayComponent,
     CubeSummarySelectorComponent,
     TextAnalysisInputComponent,
