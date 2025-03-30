975e0d1225d449b377889a5c29eace1c91902984
G Delvoye <gau.delvoye@gmail.com>
2025-03-30 17:15:59 +0200

    fix: env prod hard coded

--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: process.env['BACK_PROD_API_URL'] || 'http://localhost:5000',
+  apiUrl: 'https://mtg-cube-production.up.railway.app',
 };
