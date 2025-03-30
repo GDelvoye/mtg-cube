9044d35ffba9955a508900b7187ed83893fa5b6c
G Delvoye <gau.delvoye@gmail.com>
2025-03-30 16:59:47 +0200

    build(front): env variables.

--- front/src/app/services/visualization.service.ts
+++ front/src/app/services/visualization.service.ts
@@ -3,25 +3,31 @@ import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CubeSummary } from '../models/cube-summary.model';
 import { TextAnalysis } from '../models/text-analysis.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class VisualizationService {
-  private apiUrl = 'http://localhost:5000/visualization-official';
-  private apiUrl2 = 'http://localhost:5000/cube-text-request';
+  private apiUrl = environment.apiUrl;
   private http = inject(HttpClient);
 
   getOfficialVisualizationDta(payload: {
     setName: string;
   }): Observable<CubeSummary> {
-    return this.http.post<CubeSummary>(this.apiUrl, payload);
+    return this.http.post<CubeSummary>(
+      `${this.apiUrl}/visualization-official`,
+      payload
+    );
   }
 
   getTextAnalysis(payload: {
     text: string;
     setName: string;
   }): Observable<TextAnalysis> {
-    return this.http.post<TextAnalysis>(this.apiUrl2, payload);
+    return this.http.post<TextAnalysis>(
+      `${this.apiUrl}/cube-text-request`,
+      payload
+    );
   }
 }
---
+++ front/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: process.env['BACK_PROD_API_URL'] || 'http://localhost:5000',
+};
