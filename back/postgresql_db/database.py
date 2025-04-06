1864be3b30362cb67d37383a1bc7a5b63c441078
G Delvoye <gau.delvoye@gmail.com>
2025-04-06 18:59:03 +0200

    fix: not hard-coded DB_URL

--- back/postgresql_db/database.py
+++ back/postgresql_db/database.py
@@ -23,14 +23,8 @@ import os
 
 load_dotenv()
 
-
-# DATABASE_URL = "postgresql://mtg_user:password@localhost/mtg_cube"
-# db_url = os.getenv("DATABASE_URL")
-# engine = create_engine(db_url)
-DATABASE_URL = os.environ[
-    "DATABASE_URL_TEST"
-]  # "postgresql://postgres:JNvhXBWpVenDSFaOIEOPPlVwmlKSsETU@caboose.proxy.rlwy.net:24041/railway"
-engine = create_engine(DATABASE_URL)
+database_url = os.environ["DATABASE_URL"]
+engine = create_engine(database_url)
 
 
 class Base(DeclarativeBase):
